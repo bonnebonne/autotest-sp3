12e8d9cd036d1ca34ca562d8cae7547b6869049d
kelseyb <kelsey.bellew@mines.sdsmt.edu>
2014-03-24 11:46:35 -0600

    added header to rand_tests

--- testsuite.h
+++ testsuite.h
@@ -87,7 +87,7 @@ public:
     /*!
      * \brief Calls Menu(), finds golden cpp, calls rand_tests.
      */
-	void helper_func();
+    void helper_func();
  
     /*!
      * \brief When called, creates the summary file
@@ -129,7 +129,11 @@ private:
      */
     void menu(int& datatype, int& number_of_testcases, int& numbers_per_testcase,double& min_value,
 	double& max_value);
-	
+
+    /*!
+     * \brief Generates random tests according to the passed in 
+     * \paramaters.
+     */	
     int rand_tests(double max, double min, int type, int num_tests, int num_nums, string goldencpp); //returns 0 for success, -1 for failure
  
 	    //! Program to test.
